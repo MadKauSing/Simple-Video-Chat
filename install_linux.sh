6333675ea5b2d931a809f4a733e2a02a83e5336f
Ayush Singh <theayushsingh72@gmail.com>
2022-03-09 04:27:25 +0530

    second

--- install_linux.sh
+++ install_linux.sh
@@ -2,11 +2,16 @@ sudo apt install python3
 
 python3 -m pip install --user --upgrade pip
 python3 -m pip --version
-
 python3 -m pip install --user virtualenv
 python3 -m venv env
+
 source env/bin/activate
 which python
-python3 -m pip install requests
 
+
+python3 -m pip install requests
+python3 -m pip install pyfiglet
+python3 -m pip install opencv-python
+python3 -m pip install pytest-socket
+python3 -m pip install PyAudio
 
