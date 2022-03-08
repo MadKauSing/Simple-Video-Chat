1b42b9ffb9281b261bd222fb9f1194dd99d80024
Ayush Singh <theayushsingh72@gmail.com>
2022-03-08 11:47:50 +0530

    second commit

---
+++ run.py
@@ -0,0 +1,15 @@
+import Client;
+import Server;
+
+from multiprocessing import Process
+from time import sleep
+
+
+
+if __name__ == '__main__':
+    Process(target=Server.server).start()
+
+    sleep(1)
+    Process(target=Client.client).start()
+
+    
