41c2add8fb5624924cf2338a47495f6f18bf942b
BhaviniMadhuranath <bhaviniphillippe@gmail.com>
2022-03-13 13:07:04 +0530

    added comments

--- Client.py
+++ Client.py
@@ -1,40 +1,53 @@
-import socket
-import cv2
-import pickle
-import struct
+# client side program
+# libraries
+import socket  # library to manage socket
+import cv2  # library to capture video
+import pickle  # library to implement 'pickling' ie serialisation of a python object
+import struct  # library to pack and unpack data
 
 
 def client():
-    client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    host_ip_server = '192.168.43.162' 
+    # creation of socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host_ip_server = "192.168.43.162"
     port = 1234
     print("Socket Created Successfully")
 
-
-    client_socket.connect((host_ip_server,port))
+    # establishment of connection
+    client_socket.connect((host_ip_server, port))
     data = b""
     payload_size = struct.calcsize("Q")
     print("Socket Accepted")
 
-
+    # data transmission loop
     while True:
         print("CLIENT:")
+        # loop to receive new packet
         while len(data) < payload_size:
-            packet = client_socket.recv(2160) 
-            if not packet: break
-            data+=packet
+            packet = client_socket.recv(2160)
+            if not packet:
+                break  # error condition
+            data += packet
+        # slice packet based on payload size and store separately
         packed_msg_size = data[:payload_size]
         data = data[payload_size:]
-        msg_size = struct.unpack("Q",packed_msg_size)[0]
-        
+        # unpack the received data
+        msg_size = struct.unpack("Q", packed_msg_size)[0]
+
+        # loop to receive more data
         while len(data) < msg_size:
             data += client_socket.recv(2160)
+        # dividing data into frames
         frame_data = data[:msg_size]
-        data  = data[msg_size:]
+        data = data[msg_size:]
+        # loading and display of frames
         frame = pickle.loads(frame_data)
-        cv2.imshow("RECEIVING VIDEO",frame)
+        cv2.imshow("RECEIVING VIDEO", frame)
         key = cv2.waitKey(1) & 0xFF
-        if key  == ord('q'):
+        if key == ord("q"):
             break
+    # closing of connection
     client_socket.close()
-#client()
+
+
+# client()
